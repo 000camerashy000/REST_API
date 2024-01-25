5a42cc334f51a06c3df5f8bd424cb1f6c4e9e098
Ashish Vishwakarma <ashishworkk1@gmail.com>
2024-01-25 05:18:59 -0800

    API to print User Details

---
+++ printUserDetails.sh
@@ -0,0 +1,8 @@
+response=$(curl -X POST \
+  http://localhost:80/rest/ng/sessions \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "loginName": "av",
+    "password": "ashishOpen1",
+    "domainName": "openspecimen"
+  }')
