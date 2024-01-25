8f8bb128ff3eb351e452e5bdb4d818bccbaaee17
Ashish Vishwakarma <ashishworkk1@gmail.com>
2024-01-25 02:16:51 -0800

    Basic API to print OpenSpecimen user detail

---
+++ printUserDetails.sh
@@ -0,0 +1,8 @@
+curl -X POST \
+  http://localhost:80/rest/ng/sessions \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "loginName": "av",
+    "password": "ashishOpen1",
+    "domainName": "openspecimen"
+  }'
