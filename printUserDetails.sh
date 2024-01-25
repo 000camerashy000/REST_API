e506a083b0597cabac9f8266a8f8e938dc68cbd5
Ashish Vishwakarma <ashishworkk1@gmail.com>
2024-01-25 03:49:20 -0800

    Update printUserDetails.sh to store Token

--- printUserDetails.sh
+++ printUserDetails.sh
@@ -1,8 +1,15 @@
-curl -X POST \
+response=$(curl -X POST \
   http://localhost:80/rest/ng/sessions \
   -H 'Content-Type: application/json' \
   -d '{
     "loginName": "av",
     "password": "ashishOpen1",
     "domainName": "openspecimen"
-  }'
+  }')
+
+
+token=$(echo $response | jq -r '.token')
+
+
+my_token=$token
+echo "The token is: $my_token"
