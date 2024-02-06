75d02e6823e8470a90a1b79c496e290321d97a0c
Ashish Vishwakarma <ashishworkk1@gmail.com>
2024-02-05 19:47:35 -0800

    updated script and used methods

--- getParticipantDetails.sh
+++ getParticipantDetails.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
-getToken=$(curl -X POST \
-  http://localhost:80/rest/ng/sessions \
+
+createCsv() {
+  cat << END_OF_HEADERS > cp_registration_data.csv
+  cpTitle,ppid,firstName,activityStatus,races,ethnicities
+END_OF_HEADERS
+
+  jq -r '.[] | [.cpTitle, .ppid, .participant.firstName, .activityStatus, (.participant.races | join(",")), (.participant.ethnicities | join(","))] | @csv' <<< "$response" >> cp_registration_data.csv
+
+}
+
+getParticipants() {
+  response=$(curl -X POST \
+  'http://localhost:80/rest/ng/collection-protocol-registrations/list' \
+  -H 'Content-Type: application/json' \
+  -H "X-OS-API-TOKEN: $token" \
+  -d '{
+    "cpId": 4,
+    "maxResults": 1
+  }')
+
+  echo $response
+
+}
+
+getSessions() {
+  getToken=$(curl -X POST \
+  'http://localhost:80/rest/ng/sessions' \
   -H 'Content-Type: application/json' \
   -d '{
     "loginName": "av",
-    "password": "ashishOpen1",
+    "password": "ashishOpen1@",
     "domainName": "openspecimen"
   }')
 
+  token=$(echo $getToken | jq -r '.token')
+  echo $token
 
-token=$(echo $getToken | jq -r '.token')
+}
 
-my_token=$token
+main () {
+  getSessions
+  getParticipants
+  createCsv
 
-# Retrieve the JSON response from the API
-response=$(curl -X POST \
-  http://localhost:80/rest/ng/collection-protocol-registrations/list \
-  -H 'Content-Type: application/json' \
-  -H "X-OS-API-TOKEN: $my_token" \
-  -d '{
-    "cpId": 4,
-    "name": "AV",
-    "maxResults": 25
-  }')
+}
 
-# Extract data and convert it into a csv file.
-cat << END_OF_HEADERS > cp_registration_data.csv
-cpTitle,ppid,firstName,activityStatus,races,ethnicities
-END_OF_HEADERS
-
-jq -r '.[] | [.cpTitle, .ppid, .participant.firstName, .activityStatus, (.participant.races | join(",")), (.participant.ethnicities | join(","))] | @csv' <<< "$response" >> cp_registration_data.csv
-
-
+main;
 
